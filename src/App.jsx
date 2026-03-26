bb26ae378da0907aa71391e1bac981489ece2a89
somya singh <somya.21b0131037@abes.ac.in>
2026-03-26 15:30:16 +0530

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
 import LoanHomePage from './pages/home/Home'
 import ContactPage from './pages/contact/Page'
+import LoanServicePage from './pages/loanServices/Page'
 export default function App() {
   return (
    <Router>
@@ -11,6 +12,10 @@ export default function App() {
       }/>
       <Route path="/contact" element={
         <ContactPage/>
+      }/>
+
+      <Route path="/service" element={
+        <LoanServicePage/>
       }/>
     </Routes>
    </Router>
